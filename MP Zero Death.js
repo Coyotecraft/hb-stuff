20479b29d6d8c3d74c67500e12e14e385e29d413
Coyotecraft <64166497+Coyotecraft@users.noreply.github.com>
2024-08-24 22:16:31 -0500

    Add files via upload

--- MP Zero Death.js
+++ MP Zero Death.js
@@ -11,6 +11,8 @@
  * @url https://www.arpgmaker.com
  * @plugindesc 0MP means death
  * @author Coyotecraft 
+ * @help -The death State won't automatically set HP to 0
+ * -Mind the target scope of MP recovery items for Alive, Dead, or Unconditional. 
  */
 
 
@@ -28,6 +30,26 @@ if (this.hp === 0 || this.mp === 0) {
     } else {
         this.removeState(this.deathStateId());
     }
+};
+
+Game_Party.prototype.reviveBattleMembers = function() {
+    for (const actor of this.battleMembers()) {
+        if (actor.isDead()) {
+            if (actor.hp === 0){actor.setHp(1)} ;
+			if (actor.mp === 0){actor.setMp(1)} ;
+        }
+    }
+};
+
+Game_BattlerBase.prototype.die = function() {
+    //this._hp = 0;
+    this.clearStates();
+    this.clearBuffs();
+};
+
+Game_BattlerBase.prototype.revive = function() {
+    if (this._hp === 0) {this._hp = 1;}
+	if (this._mp === 0) {this._mp = 1;}
 };
 
 })();
